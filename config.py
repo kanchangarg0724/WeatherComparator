f6825de6e251eebc7c02a9ecb8ff6fa24842e5d8
Kanchan Garg <kanchangarg0724@gmail.com>
2020-11-09 14:47:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 # tunits: Standard unit for measuring temperature from the sources
 
 apiURL = "http://api.openweathermap.org/data/2.5/weather"
-apiKey = "3aaca3f98edc3e5d085e1da5d6981c0c"
+apiKey = "Enter your key here"
 webURL = "https://weather.com/"
 tunits = "metric" # Temperature in degree celsius
 temp_output = {}
